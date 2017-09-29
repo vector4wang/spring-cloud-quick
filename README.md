b5e7545efb58593cf880a7a9b823d71c122ef59d
vector <vector.wang@hirede.com>
2017-09-29 11:40:27 +0800

    simple

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # spring-cloud-quick
 学习springcloud,之后会将学习到的放在这里，做快速开发使用
+
+### quick-cloud-simple
+这个模块包含了最简单的栗子，Eureka中心，服务提供者与服务消费者，直接checkout运行即可
---
+++ quick-cloud-simple/simple-eureka-provider/src/main/java/com/test/provider/Application.java
@@ -0,0 +1,22 @@
+package com.test.provider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * Created with IDEA
+ * User: vector
+ * Data: 2017/9/28
+ * Time: 14:12
+ * Description:
+ */
+@EnableEurekaClient     // Eureka Client 标识
+@SpringBootApplication  // Spring Boot 应用标识
+public class Application {
+    public static void main(String[] args) {
+        // 程序启动入口
+        // 启动嵌入式的 Tomcat 并初始化 Spring 环境及其各 Spring 组件
+        SpringApplication.run(Application.class,args);
+    }
+}
