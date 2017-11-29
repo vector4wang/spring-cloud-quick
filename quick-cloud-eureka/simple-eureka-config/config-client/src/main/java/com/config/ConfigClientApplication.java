935b956b5abd1f0742c428abf84c8ca2bb3274b0
vector <vector.wang@hirede.com>
2017-11-29 19:35:54 +0800

    添加了springcloud-config模块

---
+++ quick-cloud-eureka/simple-eureka-config/config-client/src/main/java/com/config/ConfigClientApplication.java
@@ -0,0 +1,30 @@
+package com.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created with IDEA
+ * User: vector
+ * Data: 2017/11/29
+ * Time: 19:25
+ * Description:
+ */
+@SpringBootApplication
+@RestController
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+    @RequestMapping(value = "/hi")
+    public String hi(){
+        return foo;
+    }
+}
---
+++ quick-cloud-eureka/simple-eureka-config/config-server/src/main/java/com/config/ConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Created with IDEA
+ * User: vector
+ * Data: 2017/11/29
+ * Time: 19:12
+ * Description: 访问 localhost:4444/foo/dev
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
